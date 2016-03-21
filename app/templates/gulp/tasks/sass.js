3e5ebe2b431bf2da4a4a74e257200bafe4d7b044
Dan Peddle <Dan.Peddle@akqa.com>
2016-03-21 18:30:44 +0100

    sass

---
+++ app/templates/gulp/tasks/sass.js
@@ -0,0 +1,36 @@
+
+import gulp from 'gulp';
+import plumber from 'gulp-plumber';
+import notify from 'gulp-notify';
+import gutil from 'gulp-util';
+import sourcemaps from 'gulp-sourcemaps';
+import sass from 'gulp-sass';
+import base64 from 'gulp-base64';
+import cmq from 'gulp-combine-mq';
+import autoprefixer from 'gulp-autoprefixer';
+
+import config from '../config';
+
+gulp.task('sass', ['clean:css'], function() {
+    return gulp.src(config.ASSETS_SASS_MAIN)
+            .pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
+            .on('error', function(err) {
+                gutil.log(gutil.colors.red(`Error (${err.plugin}) - ${err.message}`));
+                this.emit('end');
+            })
+            .pipe(sourcemaps.init())
+            .pipe(sass())
+            .pipe(base64({
+                maxImageSize: 5000,
+                extensions: ['svg', 'png', 'jpg', /\.jpg#datauri$/i]
+            }))
+            .pipe(autoprefixer(BROWSER_CONFIG))
+            .pipe(cmq({
+                beautify: false
+            }))
+            .pipe(csso())
+            .pipe(size())
+            .pipe(sourcemaps.write())
+            .pipe(gulp.dest(STATIC_CSS))
+            .pipe(liveReload({port: LIVERELOAD_PORT}))
+});
